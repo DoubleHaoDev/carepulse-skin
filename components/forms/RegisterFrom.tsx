d6419c49fd5f19b7b760a6eca202a87433c679b0
Andy Chang <yuhaoz1991@gmail.com>
2024-07-21 15:43:33 -0700

    Implement registration page and feature

--- components/forms/RegisterFrom.tsx
+++ components/forms/RegisterFrom.tsx
@@ -7,12 +7,12 @@ import {Form, FormControl} from "@/components/ui/form"
 import CustomFormField from "@/components/CustomFormField";
 import SubmitButton from "@/components/SubmitButton";
 import {useState} from "react";
-import {UserFormValidation} from "@/lib/validation";
+import {PatientFormValidation} from "@/lib/validation";
 import {useRouter} from "next/navigation";
-import {createUser} from "@/lib/actions/patient.actions";
+import {registerPatient} from "@/lib/actions/patient.actions";
 import {FormFieldType} from "@/components/forms/PatientForm";
 import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
-import {Doctors, GenderOptions, IdentificationTypes} from "@/constants";
+import {Doctors, GenderOptions, IdentificationTypes, PatientFormDefaultValues} from "@/constants";
 import {Label} from "@/components/ui/label";
 import {SelectItem} from "@/components/ui/select";
 import Image from 'next/image';
@@ -21,23 +21,55 @@ import FileUploader from "@/components/FileUploader";
 const RegisterFrom = ({user}: { user: User }) => {
     const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
-    const form = useForm<z.infer<typeof UserFormValidation>>({
-        resolver: zodResolver(UserFormValidation),
+
+    const form = useForm<z.infer<typeof PatientFormValidation>>({
+        resolver: zodResolver(PatientFormValidation),
         defaultValues: {
+            ...PatientFormDefaultValues,
             name: "",
             email: "",
             phone: ""
         },
     })
 
-    async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
+    async function onSubmit(values: z.infer<typeof PatientFormValidation>) {
         setIsLoading(true);
-        try {
-            const userData = {name, email, phone};
-            const user = await createUser(userData);
-            if (user) {
-                router.push(`/patients/${user.$id}/register`);
+        let formData;
+        if (values.identificationDocument && values.identificationDocument.length > 0) {
+            const blobFile = new Blob([values.identificationDocument[0]], {type: values.identificationDocument[0].type});
+            formData = new FormData();
+            formData.append("blobFile", blobFile);
+            formData.append("fileName", values.identificationDocument[0].name);
+        }
 
+        try {
+            const patientData = {
+                userId: user.$id,
+                birthDate: new Date(values.birthDate),
+                identificationDocument: values.identificationDocument ? formData : undefined,
+                name: values.name,
+                email: values.email,
+                phone: values.phone,
+                gender: values.gender,
+                address: values.address,
+                occupation: values.occupation,
+                emergencyContactName: values.emergencyContactName,
+                emergencyContactNumber: values.emergencyContactNumber,
+                primaryPhysician: values.primaryPhysician,
+                insuranceProvider: values.insuranceProvider,
+                insurancePolicyNumber: values.insurancePolicyNumber,
+                allergies: values.allergies,
+                currentMedication: values.currentMedication,
+                familyMedicalHistory: values.familyMedicalHistory,
+                pastMedicalHistory: values.pastMedicalHistory,
+                identificationType: values.identificationType,
+                identificationNumber: values.identificationNumber,
+                privacyConsent: values.privacyConsent,
+            }
+
+            const patient = await registerPatient(patientData);
+            if (patient) {
+                router.push(`/patients/${user.$id}/new-appointment`)
             }
         } catch (error) {
             console.log(error);
